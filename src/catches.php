b299ad26823ad86af390c7e2bdbfbcd553aa6bcb
Daniel Rubin <cdrubin@benchmarkeducation.com>
2015-09-30 21:19:15 +0000

    Adding exception handling

--- src/catches.php
+++ src/catches.php
@@ -4,19 +4,20 @@ namespace matchmaker;
 
 function catches($value, $pattern)
 {
-    require_once('key_matcher.php');
+    require_once('key_catcher.php');
 
     if (is_array($pattern)) {
         if (!is_array($value) && !$value instanceof \Traversable) {
             throw new \Exception( "validation error: value is non-array and non-transversable" );
         }
-        $keyMatcher = key_matcher($pattern);
+        $keyCatcher = key_catcher($pattern);
         foreach ($value as $key => $item) {
-            if (!$keyMatcher($key, $item)) throw new \Exception( "validation error: value '" . $item . "' is invalid for key '" . $key . "'" );
-            if (!isset( $pattern[$key] )) throw new \Exception( "validation error: key '" . $key . "' not found in schema" );
+            if (!$keyCatcher($key, $item)) throw new \Exception( "validation error: value '" . $item . "' is invalid for key '" . $key . "'" );
+			if ($keyCatcher($key)) throw new \Exception( "validation error: key '" . $key . "' not found in schema" );
+
         }
-        if (!$keyMatcher()) return false;
-    } elseif (!matcher($value, $pattern)) {
+        if (!$keyCatcher()) return false;
+    } elseif (!catcher($value, $pattern)) {
         return false;
     }
 
