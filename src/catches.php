9eac12c83cad7572377d1e264d3f4ed76c4fc4c2
cdrubin <cdrubin@benchmarkeducation.com>
2015-10-02 12:37:39 +0000

    Improving context messages

--- src/catches.php
+++ src/catches.php
@@ -2,18 +2,18 @@
 
 namespace matchmaker;
 
-function catches($value, $pattern)
-{
+function catches($value, $pattern, $context ='') {
+
 	require_once('key_catcher.php');
 
     if (is_array($pattern)) {
         if (!is_array($value) && !$value instanceof \Traversable) {
 			return false;
         }
-        $keyCatcher = key_catcher($pattern);
+        $keyCatcher = key_catcher($pattern, $context);
         foreach ($value as $key => $item) {
-			if (!$keyCatcher($key, $item)) throw new \Exception( "validation error: value '" . $item . "' is invalid for key '" . $key . "'" );
-			if ($keyCatcher($key)) throw new \Exception( "validation error: key '" . $key . "' not found in schema" );
+			if (!$keyCatcher($key, $item) ) throw new \Exception( "validation error: invalid key '" . $context . '.' . $key . "'" );
+			if ($keyCatcher($key)) throw new \Exception( "validation error: key '" . $context . '.' . $key . "' not found in schema" );
         }
         if (!$keyCatcher()) return false;
     } elseif (!catcher($value, $pattern)) {
--- src/key_catcher.php
+++ src/key_catcher.php
@@ -13,7 +13,7 @@ require_once('catcher.php');
  * @see https://github.com/ptrofimov/matchmaker - ultra-fresh PHP matching functions
  * @author Petr Trofimov <petrofimov@yandex.ru>
  */
-function key_catcher(array $pattern)
+function key_catcher(array $pattern, $context)
 {
     $keys = [];
     foreach ($pattern as $k => $v) {
@@ -32,11 +32,11 @@ function key_catcher(array $pattern)
         array_push($keys[$k], $v, 0);
     }
 
-    return function ($key = null, $value = null) use (&$keys) {
+    return function ($key = null, $value = null) use (&$keys, $context) {
         if (is_null($key)) foreach ($keys as $count) {
             if ($count[3] < $count[0] || $count[3] > $count[1]) return false;
         } else foreach ($keys as $k => &$count) if (catcher($key, $k)) {
-            if (!catches($value, $count[2])) return false;
+            if (!catches($value, $count[2], $context . '.' . $key)) return false;
             $count[3]++;
         }
         return true;
